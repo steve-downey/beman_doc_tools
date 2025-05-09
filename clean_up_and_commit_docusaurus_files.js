d02c27d4fa18a6dc1b52d57702503ebff1705a6d
Zach Laine <whatwasthataddress@gmail.com>
2025-05-08 22:52:06 -0500

    Pass ../doc prefix to the only two JS scripts that had this path prefix hardcoded, so that the location of the ../doc Docusaurus path only ever appears in package.json.

--- clean_up_and_commit_docusaurus_files.js
+++ clean_up_and_commit_docusaurus_files.js
@@ -3,16 +3,24 @@ const path = require('path')
 const simple_git = require('simple-git')
 const git = simple_git(process.cwd())
 
+if (process.argv.length < 3) {
+    console.log(
+        process.argv[1] + ": error: Missing prefix to the files to add.")
+    process.exit(1)
+}
+
+const prefix = process.argv[2]
+
 const deletes = [
-    "../doc/blog",
-    "../doc/docs/tutorial-basics",
-    "../doc/docs/tutorial-extras",
-    "../doc/static/img/docusaurus-social-card.jpg",
-    "../doc/static/img/docusaurus.png",
-    "../doc/static/img/logo.svg",
-    "../doc/static/img/undraw_docusaurus_mountain.svg",
-    "../doc/static/img/undraw_docusaurus_react.svg",
-    "../doc/static/img/undraw_docusaurus_tree.svg",
+    path.join(prefix, "blog"),
+    path.join(prefix, "docs/tutorial-basics"),
+    path.join(prefix, "docs/tutorial-extras"),
+    path.join(prefix, "static/img/docusaurus-social-card.jpg"),
+    path.join(prefix, "static/img/docusaurus.png"),
+    path.join(prefix, "static/img/logo.svg"),
+    path.join(prefix, "static/img/undraw_docusaurus_mountain.svg"),
+    path.join(prefix, "static/img/undraw_docusaurus_react.svg"),
+    path.join(prefix, "static/img/undraw_docusaurus_tree.svg"),
 ]
 
 all_deletes = []
@@ -40,15 +48,15 @@ for (const d of deletes) {
 }
 
 const adds = [
-    "../doc/docs/api",
-    "../doc/docs/intro.md",
-    "../doc/docusaurus.config.js",
-    "../doc/sidebars.js",
-    "../doc/src/components/HomepageFeatures/index.js",
-    "../doc/src/pages/index.js",
-    "../doc/static/img/dragonduck.png",
-    "../doc/static/img/dragonduck.svg",
-    "../doc/static/img/favicon.ico",
+    path.join(prefix, "docs/api"),
+    path.join(prefix, "docs/intro.md"),
+    path.join(prefix, "docusaurus.config.js"),
+    path.join(prefix, "sidebars.js"),
+    path.join(prefix, "src/components/HomepageFeatures/index.js"),
+    path.join(prefix, "src/pages/index.js"),
+    path.join(prefix, "static/img/dragonduck.png"),
+    path.join(prefix, "static/img/dragonduck.svg"),
+    path.join(prefix, "static/img/favicon.ico"),
 ]
 
 git.rm(all_deletes).then(() => {
